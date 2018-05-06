7af40f3fc97f9b65fa8ce24fe427ad621ec30f7a
Unknown <leandrinhodalponte02@hotmail.com>
2018-05-06 17:09:59 -0300

    alteraçao
    a

--- exemplo_novo/funcionario-altera-formulario.php
+++ exemplo_novo/funcionario-altera-formulario.php
@@ -4,7 +4,7 @@
     include_once("funcionario-banco.php");
     //include_once("categorias-banco.php");
 
-    $IDFuncionario = $_GET['IDFuncionario'];
+    $IDFuncionario = $_GET['id'];
     $funcionario = buscaFuncionario($conexao, $IDFuncionario);
     //$categorias = listaCategorias($conexao);
 ?>
@@ -16,61 +16,62 @@
     <table>
         <tr>
             <td>Nome</td>
-            <td><input type="text" class="form-control" name="Nome" /></td>
+            <td><input type="text" class="form-control" value="<?= $row['Nome'] ?>" name="Nome" /></td>
         </tr>
 
         <tr>
             <td>SobreNome</td>
-            <td><input type="text" class="form-control" name="Sobrenome" /></td>
+            <td><input type="text" class="form-control" value="<?= $row['Sobrenome'] ?>" name="Sobrenome" /></td>
         </tr>
 
         <tr>
             <td>Titulo</td>
-            <td><textarea type="form-control" class="form-control" name="Titulo" ></textarea></td>
+            <td><textarea type="form-control" class="form-control" value="<?= $row['Titulo'] ?>" name="Titulo" ></textarea></td>
         </tr>
 
         <tr>
             <td>Titulo Cortesia</td>
-            <td><textarea type="form-control" class="form-control" name="TituloCortesia"></textarea></td>
+            <td><textarea type="form-control" class="form-control" value="<?= $row['TituloCortesia'] ?>" name="TituloCortesia"></textarea></td>
         </tr>
 
         <tr>
             <td>Data Nascimento</td>
-            <td><input type=date  name="DataNac" ></td>
+            <td><input type=date value="<?php echo date('Y-m-d', strtotime($row['DataNac'])); ?>" name="DataNac" ></td>
         </tr>
          
         <tr>
             <td>Data Admimissão</td>
-            <td><input type=date  name="DataAdmissao" ></td>
+            <td><input type=date value="<?php echo date('Y-m-d', strtotime($row['DataAdmissao'])); ?>" name="DataAdmissao" ></td>
         </tr>
 
         <tr>
             <td>Endereço</td>
-            <td><input type="text" class="form-control" name="Endereco" /></td>
+            <td><input type="text" value="<?= $row['Endereco'] ?>" class="form-control" name="Endereco" /></td>
         </tr>
         <tr>
             <td>Cidade</td>
-            <td><input type="text" class="form-control" name="Cidade" /></td>
+            <td><input type="text" value="<?= $row['Cidade'] ?>" class="form-control" name="Cidade" /></td>
         </tr>
         <tr>
             <td>Região</td>
-            <td><input type="text" class="form-control" name="Regiao" /></td>
+            <td><input type="text" value="<?= $row['Regiao'] ?>" class="form-control" name="Regiao" /></td>
         </tr>
         <tr>
             <td>Cep</td>
-            <td><input type="number_format" class="form-control" name="Cep" /></td>
+            <td><input type="text" value="<?= $row['Cep'] ?>" class="form-control" name="Pais" /></td>
+            <td><input type="number_format"  class="form-control" name="Cep" /></td>
         </tr>
         <tr>
             <td>Pais</td>
-            <td><input type="text" class="form-control" name="Pais" /></td>
+            <td><input type="text" value="<?= $row['Pais'] ?>" class="form-control" name="Pais" /></td>
         </tr>
         <tr>
             <td>Telefone</td>
-            <td><input type="text" class="form-control" name="TelefoneResidencial" /></td>
+            <td><input type="text" value="<?= $row['TelefoneResidencial'] ?>" class="form-control" name="TelefoneResidencial" /></td>
         </tr>
         <tr>
             <td>Extesão</td>
-            <td><input type="text" class="form-control" name="Extensao" /></td>
+            <td><input type="text" value="<?= $row['Extensao'] ?>" class="form-control" name="Extensao" /></td>
         </tr>
 
         <tr>
